1996ae8f6693580313b1fa1ec1f21011e10f3e96
0xYami <0xYami@proton.me>
2023-04-26 22:57:30 +0200

    update user routes

--- server/user.go
+++ server/user.go
@@ -10,13 +10,55 @@ import (
 	"gorm.io/gorm"
 )
 
+type userResponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		http.Error(w, "cookie not found", http.StatusUnauthorized)
+		return
+	}
+
+	db := r.Context().Value("db").(*gorm.DB)
+	user := &models.User{}
+
+	if err := db.Where("token = ?", cookie.Value).First(&user).Error; err != nil {
+		http.Error(w, "[context] failed to get user", http.StatusUnauthorized)
+		return
+	}
+
+	res := &userResponse{
+		ID:       user.ID,
+		Username: user.Username,
+		Token:    user.Token,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
+
 func userContext(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("token")
+		if err != nil {
+			http.Error(w, "[context] cookie not found", http.StatusUnauthorized)
+			return
+		}
+
+		id := chi.URLParam(r, "id")
+		if id == "" {
+			http.Error(w, "[context] missing user id", http.StatusUnauthorized)
+			return
+		}
+
 		db := r.Context().Value("db").(*gorm.DB)
-		id := chi.URLParam(r, "userID")
 		user := &models.User{}
 
-		if err := db.Where("id = ?", id).First(&user).Error; err != nil {
+		if err := db.Where("id = ? AND token = ?", id, cookie.Value).First(&user).Error; err != nil {
 			http.Error(w, "[context] failed to get user", http.StatusNotFound)
 			return
 		}
@@ -26,32 +68,15 @@ func userContext(next http.Handler) http.Handler {
 	})
 }
 
-func getUserById(w http.ResponseWriter, r *http.Request) {
+func getUser(w http.ResponseWriter, r *http.Request) {
 	user := r.Context().Value("user").(*models.User)
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(user)
 }
 
-func getAllUsers(w http.ResponseWriter, r *http.Request) {
-	var users []models.User
-	db := r.Context().Value("db").(*gorm.DB)
-	if err := db.Find(&users).Error; err != nil {
-		http.Error(w, "Failed to get users", http.StatusNotFound)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(users)
-}
-
 type createUserRequest struct {
 	Username string `json:"username" validate:"required,min=3,max=15"`
 	Password string `json:"password" validate:"required,min=8"`
-}
-
-type createUserResponse struct {
-	UserID   uint   `json:"userID"`
-	Username string `json:"username"`
-	Token    string `json:"token"`
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
@@ -74,8 +99,8 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res := &createUserResponse{
-		UserID:   user.ID,
+	res := &userResponse{
+		ID:       user.ID,
 		Username: user.Username,
 		Token:    user.Token,
 	}
