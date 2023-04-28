bcd2b977375b9bc1b66044432b75e342c571d1f8
0xYami <0xYami@proton.me>
2023-04-28 20:50:02 +0200

    add `bunker` custom color

--- ui/tailwind.config.cjs
+++ ui/tailwind.config.cjs
@@ -8,7 +8,22 @@ module.exports = {
     './nuxt.config.ts',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bunker: {
+          50: '#f3f3f4',
+          100: '#e8e8e8',
+          200: '#c5c5c6',
+          300: '#a2a3a4',
+          400: '#5c5d60',
+          500: '#16181c',
+          600: '#141619',
+          700: '#111215',
+          800: '#0d0e11',
+          900: '#0b0c0e',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
